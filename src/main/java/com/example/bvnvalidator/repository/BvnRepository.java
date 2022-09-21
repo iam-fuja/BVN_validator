09d418e83a0cf6e01cf0ce1945454d31a7c02d3d
iam-fuja <makanjuolao@gmail.com>
2022-09-21 13:04:07 +0100

    initial commit*

--- src/main/java/com/example/bvnvalidator/repository/BvnRepository.java
+++ src/main/java/com/example/bvnvalidator/repository/BvnRepository.java
@@ -1,10 +1,8 @@
 package com.example.bvnvalidator.repository;
 
 import com.example.bvnvalidator.model.Bvn;
-//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 
 @Repository
 public interface BvnRepository extends MongoRepository<Bvn, Long> {
